defdcc01bddfa660c288cb7e3dfec79dd38b4b06
Ziem0 <ziemekorlikowski@gmail.com>
2025-10-05 20:02:46 +0200

    Formatownaie

--- lab1/test_tensorflow.py
+++ lab1/test_tensorflow.py
@@ -1,3 +1,5 @@
+import os.path
+
 import keras.models
 import numpy as np
 import tensorflow as tf
@@ -11,15 +13,27 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--image", type=str)
 args = parser.parse_args()
 image_path = args.image
-# mnist = tf.keras.datasets.mnist
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
-# x_train, x_test = x_train / 255.0, x_test / 255.0
 
-model = keras.models.load_model("tutorial-model.keras")
-# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-# model.fit(x_train, y_train, epochs=5)
-# model.evaluate(x_test, y_test)
-
+if not os.path.isfile("tutorial-model.keras"):
+    model = tf.keras.models.Sequential(
+        [
+            tf.keras.layers.Flatten(input_shape=(28, 28)),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(10, activation="softmax"),
+        ]
+    )
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+    model.compile(
+        optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+    )
+    model.fit(x_train, y_train, epochs=5)
+    model.evaluate(x_test, y_test)
+    model.save("tutorial-model.keras")
+else:
+    model = keras.models.load_model("tutorial-model.keras")
 
 img_load = image.load_img(
     image_path, color_mode="grayscale", target_size=(IMG_WIDTH, IMG_HEIGHT)
