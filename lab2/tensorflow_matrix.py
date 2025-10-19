054d25806ae4185d844ca21295c6fc602bb09305
Ziem0 <ziemekorlikowski@gmail.com>
2025-10-19 16:19:21 +0200

    Formatowanie

--- lab2/tensorflow_matrix.py
+++ lab2/tensorflow_matrix.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import argparse
 import numpy as np
+
+
 @tf.function
 def rotate(points, theta):
     rotation_matrix = tf.stack(
@@ -9,6 +11,7 @@ def rotate(points, theta):
     rotation_matrix = tf.reshape(rotation_matrix, (2, 2))
     return tf.matmul(rotation_matrix, points)
 
+
 def solve_linear_system(A, B):
     det = tf.linalg.det(A)
     if det == 0:
@@ -16,6 +19,7 @@ def solve_linear_system(A, B):
         return
     X = tf.linalg.solve(A, B)
     return X
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -26,13 +30,21 @@ def main():
     args = parser.parse_args()
 
     if args.degree:
-        print(rotate(tf.constant([[2.0, 0.0]], shape=(2, 1)), np.deg2rad(args.degree, dtype=np.float32)))
+        print(
+            rotate(
+                tf.constant([[2.0, 0.0]], shape=(2, 1)),
+                np.deg2rad(args.degree, dtype=np.float32),
+            )
+        )
 
     if args.Amatrix and args.Bmatrix:
-        A = tf.constant(args.Amatrix, shape=(args.matrix_size, args.matrix_size), dtype=tf.float32)
+        A = tf.constant(
+            args.Amatrix, shape=(args.matrix_size, args.matrix_size), dtype=tf.float32
+        )
         B = tf.constant(args.Bmatrix, shape=(args.matrix_size, 1), dtype=tf.float32)
         result = solve_linear_system(A, B)
         print(result)
+
 
 if __name__ == "__main__":
     main()
